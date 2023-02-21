4b55f53d0e58751330b4a490908d81b53560405e
Mikolaj Gasior <mikolaj@gasior.dev>
2023-02-21 22:16:34 +0100

    WIP Add validation for naming conventions

--- github-workflows-validator.py
+++ github-workflows-validator.py
@@ -6,33 +6,69 @@ import yaml
 import re
 
 
-def err(s):
+def exit_with_error(s):
   sys.stderr.write(s+'\n')
   sys.exit(1)
 
 
-def v_inf(s):
+def print_warning(s):
   sys.stderr.write('!!! '+s+'\n')
 
 
-def dbg(s):
+def print_info(s):
   sys.stdout.write('*** '+s+'\n')
 
 
-def check_env_vars(vars):
+def exit_if_invalid_env_vars(vars):
   if len(vars) == 0:
     vars = ['DOT_GITHUB_PATH']
   for v in vars:
     if v not in os.environ:
-      err(v+' env var is missing')
+      exit_with_error(v+' env var is missing')
 
 
-def check_if_path_exists():
+def exit_if_invalid_path():
   if not os.environ['DOT_GITHUB_PATH'].startswith('/'):
-    err('DOT_GITHUB_PATH must be an absolute path')
+    exit_with_error('DOT_GITHUB_PATH must be an absolute path')
 
   if not os.path.isdir(os.environ['DOT_GITHUB_PATH']):
-    err('Directory from DOT_GITHUB_PATH does not exist')
+    exit_with_error('Directory from DOT_GITHUB_PATH does not exist')
+
+
+
+
+def is_lowercase_alphanumeric_with_hyphens(s):
+  return re.match(r'^[a-z0-9][a-z0-9\-]+$', s)
+
+
+def append_error_if_dict_key_missing(dict, keys, errors, err_suffix = ''):
+  for k in keys:
+    if k not in dict.keys():
+      errors.append("missing field '{0}'{1}".format(k, err_suffix))
+  return errors
+
+
+def append_error_if_dict_key_values_not_lowercase_alphanumeric_with_hyphens(dict, keys, errors):
+  for k in dict.keys():
+    if (len(keys) == 0 or k in keys) and not is_lowercase_alphanumeric_with_hyphens(dict[k]):
+      errors.append("invalid field '{0}' - should be lowercase alphanumeric with hyphens".format(k))
+  return errors
+
+
+def append_error_if_dict_key_values_contains_string(dict, keys, string, errors):
+  for k in keys:
+    if k in dict.keys() and string in dict[k]:
+      errors.append("invalid field '{0}' - should not contain '{1}'".format(k, string))
+  return errors
+
+
+def append_error_if_dict_keys_not_lowercase_alphanumeric_with_hyphens(dict, errors):
+  for k in dict.keys():
+    if not is_lowercase_alphanumeric_with_hyphens(k):
+      errors.append("invalid field '{0}' - should be lowercase alphanumeric with hyphens".format(k))
+  return errors
+
+
 
 
 def get_action_dirnames():
@@ -59,15 +95,38 @@ def get_workflow_yaml_dict(w):
   return yaml.safe_load(c)
 
 
+def get_action_yaml_dict(a):
+  action_yml = os.path.join(os.environ['DOT_GITHUB_PATH'], 'actions', a, 'action.yml')
+  invalid_action_yaml = os.path.join(os.environ['DOT_GITHUB_PATH'], 'actions', a, 'action.yaml')
+  action_yml_exists = os.path.isfile(action_yml)
+  invalid_action_yaml_exists = os.path.isfile(invalid_action_yaml)
+  if not action_yml_exists and not invalid_action_yaml_exists:
+    print_warning("cannot validate action {0} as both action.yml and action.yaml are not found".format(a))
+    return
+  if not action_yml_exists and invalid_action_yaml_exists:
+    print_warning("validating action {0} from action.yaml though it should be action.yml...".format(a))
+    action_yml = invalid_action_yaml
+
+  f = open(action_yml)
+  c = f.read()
+  f.close()
+  return yaml.safe_load(c)
+
+
+
+
 def _get_job_errors(job_dict):
   errors = []
-  # VALIDATION: Check if job has a name
-  if 'name' not in job_dict.keys():
-    errors.append('missing name')
+  # VALIDATION: step must have a 'name'
+  errors = append_error_if_dict_key_missing(job_dict, ['name'], errors)
+  # VALIDATION: if step 'id' exists, it should be lowercase alphanumeric with hyphens
+  errors = append_error_if_dict_key_values_not_lowercase_alphanumeric_with_hyphens(job_dict, ['id'], errors)
 
-  # VALIDATION: Check if job runs-on does not contain latest
-  if 'uses' not in job_dict.keys() and ('runs-on' not in job_dict.keys() or 'latest' in job_dict['runs-on']):
-    errors.append("runs-on is missing or contains 'latest'")
+  if 'uses' not in job_dict.keys():
+    # VALIDATION: if no 'uses' then 'runs-on' should exist
+    errors = append_error_if_dict_key_missing(job_dict, ['runs-on'], errors, " when no 'uses'")
+    # VALIDATION: 'runs-on' should not contain latest
+    errors = append_error_if_dict_key_values_contains_string(job_dict, ['runs-on'], "latest", errors)
 
   if 'uses' not in job_dict.keys():
     steps_errors = _get_job_steps_errors(job_dict['steps'])
@@ -108,8 +167,11 @@ def _get_job_steps_errors(steps_dict):
 
 def _get_step_errors(step_dict, job_step_outputs):
   errors = []
-  if 'name' not in step_dict.keys():
-    errors.append('missing name')
+  # VALIDATION: step must have a 'name'
+  errors = append_error_if_dict_key_missing(step_dict, ['name'], errors)
+  # VALIDATION: if step 'id' exists, it should be lowercase alphanumeric with hyphens
+  errors = append_error_if_dict_key_values_not_lowercase_alphanumeric_with_hyphens(step_dict, ['id'], errors)
+
   # VALIDATION: Calls in 'run' to non-existinging step outputs
   if 'run' in step_dict.keys():
     if isinstance(step_dict['run'], str):
@@ -117,6 +179,7 @@ def _get_step_errors(step_dict, job_step_outputs):
       if len(missing) > 0:
         for m in missing:
           errors.append("call to missing step output {0} in 'run' (or deprecated method for setting output used)".format(m))
+
   # VALIDATION: Calls in 'env' or 'with' to non-existinging step outputs
   for key_to_check in ['env', 'with']:
     if key_to_check in step_dict.keys():
@@ -144,8 +207,18 @@ def _get_missing_step_outputs(contents, job_step_outputs):
 def get_errors_from_workflow(w):
   errors = []
   y = get_workflow_yaml_dict(w)
+
+  # VALIDATION: workflow must have a 'name'
+  errors = append_error_if_dict_key_missing(y, ['name'], errors)
+  # VALIDATION: job name should be lowercase alphanumeric with 
+  errors = append_error_if_dict_keys_not_lowercase_alphanumeric_with_hyphens(y['jobs'], errors)
+  
   job_names = y['jobs'].keys()
-  # TODO: validate job names
+  # VALIDATION: if there is only one job in the workflow then it should be named 'main'
+  if len(job_names) == 1 and list(job_names)[0] != 'main':
+    errors.append("job '{0}' is the only job in the workflow and should be named 'main'".format(list(job_names)[0]))
+
+  # Loop through jobs and validate them
   for job_name in job_names:
     job_errors = _get_job_errors(y['jobs'][job_name])
     if len(job_errors) > 0:
@@ -154,24 +227,88 @@ def get_errors_from_workflow(w):
   return errors
 
 
+def get_errors_from_action(a):
+  errors = []
+  y = get_action_yaml_dict(a)
+  # VALIDATION: action must have 'name' and 'description' fields
+  errors = append_error_if_dict_key_missing(y, ['name', 'description'], errors)
+  return errors
+
+
+def get_errors_from_action_filenames(dirnames):
+  errors = []
+  for dir in dirnames:
+    # VALIDATION: action should have action.yml (and not .yaml)
+    action_yml = os.path.join(os.environ['DOT_GITHUB_PATH'], 'actions', dir, 'action.yml')
+    invalid_action_yaml = os.path.join(os.environ['DOT_GITHUB_PATH'], 'actions', dir, 'action.yaml')
+    action_yml_exists = os.path.isfile(action_yml)
+    invalid_action_yaml_exists = os.path.isfile(invalid_action_yaml)
+    if not action_yml_exists and invalid_action_yaml_exists:
+      errors.append("invalid extension - expected {0} but found {1}".format(action_yml, invalid_action_yaml))
+    if not action_yml_exists and not invalid_action_yaml_exists:
+      errors.append("missing or invalid action file - {0} (nor invalid action.yaml) not found".format(action_yml))
+    if action_yml_exists and invalid_action_yaml_exists:
+      errors.append("duplicated action file - found both valid {0} and invalid {1}".format(action_yml, invalid_action_yaml))
+
+    # VALIDATION: action dir name should have lower case alphanumeric and hyphen only
+    if not is_lowercase_alphanumeric_with_hyphens(dir):
+      errors.append("invalid action dir name '{0}' - should be lowercase alphanumeric with hyphens".format(dir))
+
+  return errors
+
+
+def get_errors_from_workflow_filenames(filenames):
+  errors = []
+  for f in filenames:
+    # VALIDATION: .yml extension
+    if not f.endswith('.yml'):
+      errors.append("workflow {0} should end with .yml".format(f))
+
+    # VALIDATION: should not contain underscore
+    if '_' in f:
+      errors.append("workflow {0} filename should not contain underscore - use hyphens".format(f))
+
+    # VALIDATION: workflow filename can start with underscore and then it should be lower case alphanumeric and hyphen only
+    if not re.match(r'^[_]{0,1}[a-z0-9][a-z0-9\-]+\.yml$', f):
+      errors.append("invalid workflow filename '{0}' - should be '^[_]{{0,1}}[a-z0-9][a-z0-9\-]+\.yml$'".format(f))
+
+  return errors
+
+
 def main():
-  check_env_vars([])
-  check_if_path_exists()
+  exit_if_invalid_env_vars([])
+  exit_if_invalid_path()
 
   action_dirnames = get_action_dirnames()
   workflow_filenames = get_workflow_filenames()
-  dbg('Found action dirnames: ' + ', '.join(action_dirnames))
-  dbg('Found workflow filenames: ' + ', '.join(workflow_filenames))
+  print_info('Found action dirnames: ' + ', '.join(action_dirnames))
+  print_info('Found workflow filenames: ' + ', '.join(workflow_filenames))
 
-  # TODO: validate action dirnames
-  # TODO: validate action.yaml files
-  # TODO: validate workflow filenames
+  # Validate action dir name and file inside
+  errors = get_errors_from_action_filenames(action_dirnames)
+  if len(errors) > 0:
+    for err in errors:
+      print_warning('action filenames -> {0}'.format(err))
 
+  # Validate workflow file names
+  errors = get_errors_from_workflow_filenames(workflow_filenames)
+  if len(errors) > 0:
+    for err in errors:
+      print_warning('workflow filenames -> {0}'.format(err))
+
+  # Loop through actions and validate them
+  for a in action_dirnames:
+    action_errors = get_errors_from_action(a)
+    if len(action_errors) > 0:
+      for err in action_errors:
+        print_warning('action {0} -> {1}'.format(a, err))
+  
+  # Loop through workflows and validate them
   for w in workflow_filenames:
     workflow_errors = get_errors_from_workflow(w)
     if len(workflow_errors) > 0:
       for err in workflow_errors:
-        v_inf('workflow {0} -> {1}'.format(w, err))
+        print_warning('workflow {0} -> {1}'.format(w, err))
 
   # TODO: make script fail when there are any errors
 
