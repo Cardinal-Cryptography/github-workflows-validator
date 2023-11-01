c5b9c834003239dc4c11c296a67bd02a10dfffdd
Mikolaj Gasior <mikolaj@gasior.dev>
2023-11-01 19:22:39 +0100

    Update cli library

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM golang:alpine AS builder
-LABEL maintainer="Mikolaj Gasior"
+LABEL maintainer="devops@alephzero"
 
 RUN apk add --update git bash openssh make gcc musl-dev
 
--- main.go
+++ main.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"fmt"
-	gocli "github.com/MikolajGasior/go-mod-cli"
+	gocli "github.com/bitsnops/go-broccli"
 	"os"
 
 	"github.com/Cardinal-Cryptography/github-actions-validator/pkg/dotgithub"
 )
 
 func main() {
-	cli := gocli.NewCLI("github-actions-validator", "Validates GitHub Actions' .github directory", "Mikolaj Gasior <mikolaj@gasior.dev>")
+	cli := gocli.NewCLI("github-actions-validator", "Validates GitHub Actions' .github directory", "devops@alephzero")
 	cmdValidate := cli.AddCmd("validate", "Runs the validation on files from a specified directory", validateHandler)
 	cmdValidate.AddFlag("path", "p", "", "Path to .github directory", gocli.TypePathDir|gocli.MustExist|gocli.Required, nil)
 	cmdValidate.AddFlag("vars-file", "z", "", "Check if variable names exist in this file (one per line)", gocli.TypePathFile|gocli.MustExist, nil)
