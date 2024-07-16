0ce0019dd7e22934c74c4bd0c425e0c811426110
Mikolaj Gasior <g@f0x.pl>
2024-07-16 21:28:09 +0200

    Update cli lib path

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/Cardinal-Cryptography/github-actions-validator
 go 1.19
 
 require (
-	github.com/nicholasgasior/go-broccli v2.0.0+incompatible
+	github.com/mikolajgs/broccli v2.0.0+incompatible
 	gopkg.in/yaml.v2 v2.4.0
 )
--- main.go
+++ main.go
@@ -2,18 +2,18 @@ package main
 
 import (
 	"fmt"
-	gocli "github.com/nicholasgasior/go-broccli"
+	"github.com/mikolajgs/broccli"
 	"os"
 
 	"github.com/Cardinal-Cryptography/github-actions-validator/pkg/dotgithub"
 )
 
 func main() {
-	cli := gocli.NewCLI("github-actions-validator", "Validates GitHub Actions' .github directory", "devops@cc")
+	cli := broccli.NewCLI("github-actions-validator", "Validates GitHub Actions' .github directory", "infra-team@cardinals")
 	cmdValidate := cli.AddCmd("validate", "Runs the validation on files from a specified directory", validateHandler)
-	cmdValidate.AddFlag("path", "p", "", "Path to .github directory", gocli.TypePathFile, gocli.IsDirectory|gocli.IsExistent|gocli.IsRequired)
-	cmdValidate.AddFlag("vars-file", "z", "", "Check if variable names exist in this file (one per line)", gocli.TypePathFile, gocli.IsExistent)
-	cmdValidate.AddFlag("secrets-file", "s", "", "Check if secret names exist in this file (one per line)", gocli.TypePathFile, gocli.IsExistent)
+	cmdValidate.AddFlag("path", "p", "", "Path to .github directory", broccli.TypePathFile, broccli.IsDirectory|broccli.IsExistent|broccli.IsRequired)
+	cmdValidate.AddFlag("vars-file", "z", "", "Check if variable names exist in this file (one per line)", broccli.TypePathFile, broccli.IsExistent)
+	cmdValidate.AddFlag("secrets-file", "s", "", "Check if secret names exist in this file (one per line)", broccli.TypePathFile, broccli.IsExistent)
 	_ = cli.AddCmd("version", "Prints version", versionHandler)
 	if len(os.Args) == 2 && (os.Args[1] == "-v" || os.Args[1] == "--version") {
 		os.Args = []string{"App", "version"}
@@ -21,12 +21,12 @@ func main() {
 	os.Exit(cli.Run())
 }
 
-func versionHandler(c *gocli.CLI) int {
+func versionHandler(c *broccli.CLI) int {
 	fmt.Fprintf(os.Stdout, VERSION+"\n")
 	return 0
 }
 
-func validateHandler(c *gocli.CLI) int {
+func validateHandler(c *broccli.CLI) int {
 	dotGithub := dotgithub.DotGithub{
 		Path:        c.Flag("path"),
 		VarsFile:    c.Flag("vars-file"),
